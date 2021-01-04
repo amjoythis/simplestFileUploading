f5b9dbce2ed8326287a1fa5c54e725c482728665
Artur Marques <am@arturmarques.com>
2021-01-04 21:24:08 +0000

    very basic example of handling file uploads in PHP

---
+++ simplest_file_receiver.php
@@ -0,0 +1,78 @@
+<?php
+
+var_dump ($_FILES);
+
+var_dump ($_POST);
+
+function receiveSingleFile(
+    string $pIdOfFileElement,
+    string $pStrDestinationDir = "./uploads"
+){
+    $strName = $_FILES[$pIdOfFileElement]['name'];
+    $iSize = $_FILES[$pIdOfFileElement]['size'];
+    $strTmpName = $_FILES[$pIdOfFileElement]['tmp_name'];
+    $iError = $_FILES[$pIdOfFileElement]['error'];
+    $strType = $_FILES[$pIdOfFileElement]['type'];
+
+    if ($iError===0){
+        @mkdir(
+            $pStrDestinationDir,
+            777,
+            true
+        );
+        $iMoveResult =
+            move_uploaded_file(
+                $strTmpName,
+                $pStrDestinationDir."/".$strName
+            );
+
+        if ($iMoveResult!==false){
+            return $iSize;
+        }
+    }
+
+    return false;
+}//receiveSingleFile
+
+function receiveMultipleFiles(
+    string $pIdOfMultipleFileElement,
+    string $pStrDestinationDir = "./uploads"
+){
+    $aRet = [];
+    $bCheck = is_array ($_FILES[$pIdOfMultipleFileElement]['name']);
+    if ($bCheck){
+        @mkdir(
+            $pStrDestinationDir,
+            777,
+            true
+        );
+
+        for ($idx=0 ; $idx<count($_FILES[$pIdOfMultipleFileElement]['name']); $idx++){
+            $strName = $_FILES[$pIdOfMultipleFileElement]['name'][$idx];
+            $iSize = $_FILES[$pIdOfMultipleFileElement]['size'][$idx];
+            $strTmpName = $_FILES[$pIdOfMultipleFileElement]['tmp_name'][$idx];
+            $iError = $_FILES[$pIdOfMultipleFileElement]['error'][$idx];
+            $strType = $_FILES[$pIdOfMultipleFileElement]['type'][$idx];
+
+            if ($iError===0){
+                $iMoveResult =
+                    move_uploaded_file(
+                        $strTmpName,
+                        $pStrDestinationDir."/".$strName
+                    );
+
+                $aRet[$strTmpName] = $iMoveResult;
+            }//if
+            else{
+                $aRet[$strTmpName] = false;
+            }
+        }//for
+    }//if check multiple
+
+    return $aRet;
+}//receiveMultipleFiles
+
+foreach($_FILES as $strNameOfFileElement => $aDataOfFileElement){
+    //receiveSingleFile($strNameOfFileElement);
+    receiveMultipleFiles($strNameOfFileElement);
+}//foreach
